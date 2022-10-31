c9399ac3809a502443ee0469947290598126d7b7
Amin Febryansyah <aminfebryansyah240203@gmail.com>
2022-10-31 17:03:53 +0700

    1st Algorithm

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -21,10 +21,50 @@
             Console.WriteLine(".....................");
             Console.WriteLine("Masukkan elemet Array");
             Console.WriteLine(".....................");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
+        }
+        public void display()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("......................");
+            Console.WriteLine("Shorted array elements");
+            Console.WriteLine("......................");
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+        }
+        public void SelectionSortArray()
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (a[j] < a[min])
+                    {
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[min];
+                        a[min] = temp;
+                    }
+                }
+            }
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program myList = new Program();
+            myList.read();
+            myList.SelectionSortArray();
+            myList.display();
+            Console.WriteLine("\n\nPress Return to exit.");
+            Console.Read();
         }
     }
 }
