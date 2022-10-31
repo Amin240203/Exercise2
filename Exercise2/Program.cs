31a99a98d56d263733b5bd44ddf3f24c331a7b5a
Amin Febryansyah <aminfebryansyah240203@gmail.com>
2022-10-31 16:46:45 +0700

    Add project files.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise2
+{
+    internal class Program
+    {
+        //deinisika array dengan ukuran maksimun
+        private int[] a = new int[36];
+        private int n;
+        public void read()
+        {
+            while (true)
+            {
+                Console.WriteLine("Masukkan jumlah Element/Data array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray maksimum 20 element\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine(".....................");
+            Console.WriteLine("Masukkan elemet Array");
+            Console.WriteLine(".....................");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
